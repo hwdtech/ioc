cf140796b7689afd1867581377bb582b0f4e6b85
IvanMolodtsov <vanmolodtsov@gmail.com>
2019-08-01 18:30:29 +0600

    fixed npm packaging

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import IoC from "./IoC";
-import Scope from "./Scope";
+import IoC from "./src/IoC";
+import Scope from "./src/Scope";
 
 const RootScope: Scope = {
     body: {},
     id: 0
 };
+
+console.log("da")
 
 const ioc = new IoC(RootScope);
 export default ioc;
